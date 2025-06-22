572313c8982776514fc098fc5f72f7cb1e1403ed
MegaBit016 <megalodon016@gmail.com>
2025-06-22 05:46:43 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -126,6 +126,15 @@ import { continueCode, continueCodeFindIt, continueCodeFixIt } from './routes/co
 import { ensureFileIsPassed, handleZipFileUpload, checkUploadSize, checkFileType, handleXmlUpload, handleYamlUpload } from './routes/fileUpload'
 
 const app = express()
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+
+  next()
+})
+
 const server = new http.Server(app)
 
 // errorhandler requires us from overwriting a string property on it's module which is a big no-no with esmodules :/
